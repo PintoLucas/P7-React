cdcfe23bc04ab56c4af0bc8863046e7b50103243
Lucas <lucas-pinto@outlook.fr>
2022-06-24 11:46:24 +0200

    banner, cart and shoppinglist

---
+++ la-maison-jungle/src/components/Banner.js
@@ -0,0 +1,18 @@
+import logo from '../assets/logo.png'
+import '../styles/Banner.css'
+import Recommendation from './Recommendation'
+
+function Banner() {
+    const title = 'La maison jungle'
+    return (
+        <div className='lmj-banner'>
+            <div className='lmj-banner-row'>
+                <img src={logo} alt='La maison jungle' className='lmj-logo' />
+                <h1>{title}</h1>
+            </div>
+            <Recommendation />
+        </div>
+    )
+}
+
+export default Banner
