b910a8b69bf34058431269714565310cde3c6fab
Lucas <lucas-pinto@outlook.fr>
2022-06-26 21:33:15 +0200

    props and events

--- la-maison-jungle/src/components/Banner.js
+++ la-maison-jungle/src/components/Banner.js
@@ -1,18 +1,7 @@
-import logo from '../assets/logo.png'
 import '../styles/Banner.css'
-import Recommendation from './Recommendation'
 
-function Banner() {
-    const title = 'La maison jungle'
-    return (
-        <div className='lmj-banner'>
-            <div className='lmj-banner-row'>
-                <img src={logo} alt='La maison jungle' className='lmj-logo' />
-                <h1>{title}</h1>
-            </div>
-            <Recommendation />
-        </div>
-    )
+function Banner({ children }) {
+    return <div className='lmj-banner'>{children}</div>
 }
 
 export default Banner
